db57210a0e32835e28d9d246a88a464d66fa0898
Kotimäki Juuso K <jkotimak@vdi-cubic-019.ad.helsinki.fi>
2020-11-20 14:10:56 +0200

    javadoc for approx

--- src/main/java/travelleri/domain/ApproxTSP.java
+++ src/main/java/travelleri/domain/ApproxTSP.java
@@ -1,19 +1,37 @@
 package travelleri.domain;
 
+/**
+ * Pyrkii etsimään lyhyen polun syötetystä verkosta niin, että polku kulkee kaikkien
+ * verkon solmujen kautta palaten takaisin lähtösolmuun. Algoritmin approksimoiva
+ * toteutus (ei palauta optimaalista tulosta, mutta on suorituskykyinen).
+ */
 public class ApproxTSP implements TSP {
-    private int nodesCount;
-    private double[][] graph;
-    private boolean[] included;
+    private double[][] graph; // solmujen määrä
+    private int nodesCount; // verkko
+    private boolean[] visited; // primMST apumuuttuja (onko solmussa käyty)
     private double shortestPathLength; // laskettu lyhyimmän polun pituus
     private int[] shortestPath; // laskettu verkon lyhyin polku
     private boolean ran; // onko algoritmi suoritettu
 
+    /**
+     * Konstruktori.
+     *
+     * @param graph välimatkoista koostuva n*n matriisi, graph[x][y] on x:n ja y:n
+     *              välinen etäisyys liukulukuna
+     */
     public ApproxTSP(double[][] graph) {
         this.graph = graph;
         this.nodesCount = graph.length;
-        this.included = new boolean[graph.length];
+        this.visited = new boolean[graph.length];
     }
 
+    /**
+     * Lisää arvon listan loppuun. Apumetodi primMST() -metodille.
+     * 
+     * @param array lista, johon arvo lisätään
+     * @param value arvo, joka lisätään listaan
+     * @return uusi lista, johon arvo on lisätty
+     */
     private int[] arrayAppend(int[] array, int value) {
         int[] newArray = new int[array.length + 1];
         System.arraycopy(array, 0, newArray, 0, array.length);
@@ -21,15 +39,26 @@ public class ApproxTSP implements TSP {
 
         return newArray;
     }
-
+    /**
+     * Toteuttaa Primin algoritmin mukaisen verkon läpikäynnin,
+     * jossa valitaan aina solmun lähimpänä oleva naapuri ja edetään sinne
+     * 
+     * currentPath ja currentPathLength ovat "lisänä" ns. lyhimmän polun solmujen
+     * kirjanpitoa varten. Tallentaa lopputuloksen oliomuuttujiin shortestPath ja
+     * shortestPathLength, kun kaikki verkon solmut on käyty läpi.
+     * 
+     * @param currentNode       aloitussolmu
+     * @param currentPath       tämänhetkinen polku
+     * @param currentPathLength tämänhetkinen polun pituus
+     */
     private void primMST(int currentNode, int[] currentPath, double currentPathLength) {
-        included[currentNode] = true;
+        visited[currentNode] = true;
 
         double minWeight = Double.MAX_VALUE;
         int closestNeighbor = -1;
 
         for (int node = 0; node < nodesCount; node++) {
-            if (included[node]) {
+            if (visited[node]) {
                 continue;
             }
 
@@ -49,7 +78,9 @@ public class ApproxTSP implements TSP {
         primMST(closestNeighbor, arrayAppend(currentPath, currentNode), currentPathLength+minWeight);
     }
 
-
+    /**
+     * Suorittaa algoritmin.
+     */
     @Override
     public void run() {
         primMST(0, new int[0], 0);
@@ -83,6 +114,4 @@ public class ApproxTSP implements TSP {
         }
         return shortestPathLength;
     }
-
-    
 }
